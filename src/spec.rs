52d7a792dd6215ad34209edf320374b27344974d
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-06-20 22:44:29 +1000

    Parse ranges in Spec

--- src/spec.rs
+++ src/spec.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use serde_with::{serde_as, DisplayFromStr};
+
 use crate::midi;
 
 #[derive(serde::Deserialize, Debug, Clone)]
@@ -8,11 +12,37 @@ pub enum Spec {
 
 /// Represents a specification for a number or range of numbers,
 /// e.g. the velocity value of a MIDI note on message.
+#[serde_as]
 #[derive(serde::Deserialize, Debug, Clone, Default)]
 #[serde(untagged)]
 pub enum Number {
     #[default]
     Any,
     Value(u32),
-    Range(String),
+    Range(#[serde_as(as = "DisplayFromStr")] Range),
+}
+
+/// An inclusive range between two numbers,
+/// parsed from e.g. `3-10`.
+#[derive(Debug, Clone, Default)]
+pub struct Range(u32, u32);
+
+impl FromStr for Range {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.trim().split('-');
+        let a = iter.next().ok_or("No `-`")?.trim();
+        let b = iter.next().ok_or("No number after `-`")?.trim();
+        if iter.next().is_none() {
+            Ok(Self(
+                a.parse()
+                    .map_err(|_| "Couldn't parse number on left of `-`")?,
+                b.parse()
+                    .map_err(|_| "Couldn't parse number on right of `-`")?,
+            ))
+        } else {
+            Err("Too many `-`s")
+        }
+    }
 }
