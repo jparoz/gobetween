d9e7ebb30a1f71cfa96398fb98e888d26cc1b276
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-06-23 23:54:33 +1000

    Account for MIDI channels in midi::spec

--- src/midi/spec.rs
+++ src/midi/spec.rs
@@ -11,12 +11,18 @@ pub enum Spec {
     NoteOn {
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
         note: Vec<spec::Number>,
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
         velocity: Vec<spec::Number>,
     },
     NoteOff {
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
         note: Vec<spec::Number>,
@@ -27,6 +33,9 @@ pub enum Spec {
     ControlChange {
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
         controller: Vec<spec::Number>,
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
@@ -35,9 +44,15 @@ pub enum Spec {
     ProgramChange {
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
         program: Vec<spec::Number>,
     },
     PolyPressure {
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
         note: Vec<spec::Number>,
@@ -48,9 +63,15 @@ pub enum Spec {
     ChannelPressure {
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
         pressure: Vec<spec::Number>,
     },
     PitchBend {
+        #[serde(default)]
+        #[serde_as(as = "OneOrMany<_>")]
+        channel: Vec<spec::Number>,
         #[serde(default)]
         #[serde_as(as = "OneOrMany<_>")]
         bend: Vec<spec::Number>,
@@ -65,57 +86,110 @@ impl Matches for Spec {
     /// and if it does,
     /// returns a [`Match`] describing the qualities of the match.
     fn matches(&self, live_event: LiveEvent<'static>) -> Option<Match> {
-        let msg = match live_event {
-            // @Todo: use channel
-            LiveEvent::Midi { channel, message } => message,
-            _ => return None,
-        };
+        let (channel, message) = match_ok!(
+            live_event,
+            LiveEvent::Midi {
+                channel: _0,
+                message: _1
+            }
+        )?;
 
-        match msg {
+        match message {
             MidiMessage::NoteOn { key, vel } => {
-                let spec = match_ok!(self, Spec::NoteOn { note, velocity })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::NoteOn {
+                        channel,
+                        note,
+                        velocity
+                    }
+                )?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
                 let note = spec::matches_many(spec.note, key.as_int() as u32)?;
                 let velocity = spec::matches_many(spec.velocity, vel.as_int() as u32)?;
-                Some(Match::NoteOn { note, velocity })
+                Some(Match::NoteOn {
+                    channel,
+                    note,
+                    velocity,
+                })
             }
 
             MidiMessage::NoteOff { key, vel } => {
-                let spec = match_ok!(self, Spec::NoteOff { note, velocity })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::NoteOff {
+                        channel,
+                        note,
+                        velocity
+                    }
+                )?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
                 let note = spec::matches_many(spec.note, key.as_int() as u32)?;
                 let velocity = spec::matches_many(spec.velocity, vel.as_int() as u32)?;
-                Some(Match::NoteOff { note, velocity })
+                Some(Match::NoteOff {
+                    channel,
+                    note,
+                    velocity,
+                })
             }
 
             MidiMessage::Aftertouch { key, vel } => {
-                let spec = match_ok!(self, Spec::PolyPressure { note, pressure })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::PolyPressure {
+                        channel,
+                        note,
+                        pressure
+                    }
+                )?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
                 let note = spec::matches_many(spec.note, key.as_int() as u32)?;
                 let pressure = spec::matches_many(spec.pressure, vel.as_int() as u32)?;
-                Some(Match::PolyPressure { note, pressure })
+                Some(Match::PolyPressure {
+                    channel,
+                    note,
+                    pressure,
+                })
             }
 
             MidiMessage::Controller { controller, value } => {
-                let spec = match_ok!(self, Spec::ControlChange { controller, value })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::ControlChange {
+                        channel,
+                        controller,
+                        value
+                    }
+                )?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
                 let controller = spec::matches_many(spec.controller, controller.as_int() as u32)?;
                 let value = spec::matches_many(spec.value, value.as_int() as u32)?;
-                Some(Match::ControlChange { controller, value })
+                Some(Match::ControlChange {
+                    channel,
+                    controller,
+                    value,
+                })
             }
 
             MidiMessage::ProgramChange { program } => {
-                let spec = match_ok!(self, Spec::ProgramChange { program })?;
-                let program = spec::matches_many(spec, program.as_int() as u32)?;
-                Some(Match::ProgramChange { program })
+                let spec = match_ok!(self, Spec::ProgramChange { channel, program })?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
+                let program = spec::matches_many(spec.program, program.as_int() as u32)?;
+                Some(Match::ProgramChange { channel, program })
             }
 
             MidiMessage::ChannelAftertouch { vel } => {
-                let spec = match_ok!(self, Spec::ChannelPressure { pressure })?;
-                let pressure = spec::matches_many(spec, vel.as_int() as u32)?;
-                Some(Match::ChannelPressure { pressure })
+                let spec = match_ok!(self, Spec::ChannelPressure { channel, pressure })?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
+                let pressure = spec::matches_many(spec.pressure, vel.as_int() as u32)?;
+                Some(Match::ChannelPressure { channel, pressure })
             }
 
             MidiMessage::PitchBend { bend } => {
-                let spec = match_ok!(self, Spec::PitchBend { bend })?;
-                let bend = spec::matches_many(spec, bend.0.as_int() as u32)?;
-                Some(Match::PitchBend { bend })
+                let spec = match_ok!(self, Spec::PitchBend { channel, bend })?;
+                let channel = spec::matches_many(spec.channel, channel.as_int() as u32)?;
+                let bend = spec::matches_many(spec.bend, bend.0.as_int() as u32)?;
+                Some(Match::PitchBend { channel, bend })
             }
         }
     }
@@ -125,11 +199,23 @@ impl Matches for Spec {
     fn generate(&self, matched: Match) -> Option<LiveEvent<'static>> {
         match matched {
             Match::NoteOn {
+                channel: (channel_ix, channel_match),
                 note: (note_ix, note_match),
                 velocity: (velocity_ix, velocity_match),
             } => {
-                let spec = match_ok!(self, Spec::NoteOn { note, velocity })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::NoteOn {
+                        channel,
+                        note,
+                        velocity
+                    }
+                )?;
 
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
                 let key = spec.note.get(note_ix as usize)?.generate(note_match)?;
                 let vel = spec
                     .velocity
@@ -137,8 +223,7 @@ impl Matches for Spec {
                     .generate(velocity_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::NoteOn {
                         key: (key as u8).into(),
                         vel: (vel as u8).into(),
@@ -147,11 +232,23 @@ impl Matches for Spec {
             }
 
             Match::NoteOff {
+                channel: (channel_ix, channel_match),
                 note: (note_ix, note_match),
                 velocity: (velocity_ix, velocity_match),
             } => {
-                let spec = match_ok!(self, Spec::NoteOff { note, velocity })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::NoteOff {
+                        channel,
+                        note,
+                        velocity
+                    }
+                )?;
 
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
                 let key = spec.note.get(note_ix as usize)?.generate(note_match)?;
                 let vel = spec
                     .velocity
@@ -159,8 +256,7 @@ impl Matches for Spec {
                     .generate(velocity_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::NoteOff {
                         key: (key as u8).into(),
                         vel: (vel as u8).into(),
@@ -169,11 +265,23 @@ impl Matches for Spec {
             }
 
             Match::ControlChange {
+                channel: (channel_ix, channel_match),
                 controller: (controller_ix, controller_match),
                 value: (value_ix, value_match),
             } => {
-                let spec = match_ok!(self, Spec::ControlChange { controller, value })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::ControlChange {
+                        channel,
+                        controller,
+                        value
+                    }
+                )?;
 
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
                 let controller = spec
                     .controller
                     .get(controller_ix as usize)?
@@ -181,8 +289,7 @@ impl Matches for Spec {
                 let value = spec.value.get(value_ix as usize)?.generate(value_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::Controller {
                         controller: (controller as u8).into(),
                         value: (value as u8).into(),
@@ -191,11 +298,23 @@ impl Matches for Spec {
             }
 
             Match::PolyPressure {
+                channel: (channel_ix, channel_match),
                 note: (note_ix, note_match),
                 pressure: (pressure_ix, pressure_match),
             } => {
-                let spec = match_ok!(self, Spec::PolyPressure { note, pressure })?;
+                let spec = match_ok!(
+                    self,
+                    Spec::PolyPressure {
+                        channel,
+                        note,
+                        pressure
+                    }
+                )?;
 
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
                 let key = spec.note.get(note_ix as usize)?.generate(note_match)?;
                 let vel = spec
                     .pressure
@@ -203,8 +322,7 @@ impl Matches for Spec {
                     .generate(pressure_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::Aftertouch {
                         key: (key as u8).into(),
                         vel: (vel as u8).into(),
@@ -213,15 +331,22 @@ impl Matches for Spec {
             }
 
             Match::ProgramChange {
+                channel: (channel_ix, channel_match),
                 program: (program_ix, program_match),
             } => {
-                let spec = match_ok!(self, Spec::ProgramChange { program })?;
+                let spec = match_ok!(self, Spec::ProgramChange { channel, program })?;
 
-                let program = spec.get(program_ix as usize)?.generate(program_match)?;
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
+                let program = spec
+                    .program
+                    .get(program_ix as usize)?
+                    .generate(program_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::ProgramChange {
                         program: (program as u8).into(),
                     },
@@ -229,15 +354,22 @@ impl Matches for Spec {
             }
 
             Match::ChannelPressure {
+                channel: (channel_ix, channel_match),
                 pressure: (pressure_ix, pressure_match),
             } => {
-                let spec = match_ok!(self, Spec::ProgramChange { program })?;
+                let spec = match_ok!(self, Spec::ChannelPressure { channel, pressure })?;
 
-                let vel = spec.get(pressure_ix as usize)?.generate(pressure_match)?;
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
+                let vel = spec
+                    .pressure
+                    .get(pressure_ix as usize)?
+                    .generate(pressure_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::ChannelAftertouch {
                         vel: (vel as u8).into(),
                     },
@@ -245,15 +377,19 @@ impl Matches for Spec {
             }
 
             Match::PitchBend {
+                channel: (channel_ix, channel_match),
                 bend: (bend_ix, bend_match),
             } => {
-                let spec = match_ok!(self, Spec::PitchBend { bend })?;
+                let spec = match_ok!(self, Spec::PitchBend { channel, bend })?;
 
-                let bend = spec.get(bend_ix as usize)?.generate(bend_match)?;
+                let channel = spec
+                    .channel
+                    .get(channel_ix as usize)?
+                    .generate(channel_match)?;
+                let bend = spec.bend.get(bend_ix as usize)?.generate(bend_match)?;
 
                 Some(LiveEvent::Midi {
-                    // @XXX @Fixme: do something with channel
-                    channel: 0.into(),
+                    channel: (channel as u8).into(),
                     message: MidiMessage::PitchBend {
                         bend: midly::PitchBend((bend as u16).into()),
                     },
@@ -270,28 +406,35 @@ impl Matches for Spec {
 #[derive(Debug, Clone)]
 pub enum Match {
     NoteOn {
+        channel: (u32, spec::NumberMatch),
         note: (u32, spec::NumberMatch),
         velocity: (u32, spec::NumberMatch),
     },
     NoteOff {
+        channel: (u32, spec::NumberMatch),
         note: (u32, spec::NumberMatch),
         velocity: (u32, spec::NumberMatch),
     },
     ControlChange {
+        channel: (u32, spec::NumberMatch),
         controller: (u32, spec::NumberMatch),
         value: (u32, spec::NumberMatch),
     },
     ProgramChange {
+        channel: (u32, spec::NumberMatch),
         program: (u32, spec::NumberMatch),
     },
     PolyPressure {
+        channel: (u32, spec::NumberMatch),
         note: (u32, spec::NumberMatch),
         pressure: (u32, spec::NumberMatch),
     },
     ChannelPressure {
+        channel: (u32, spec::NumberMatch),
         pressure: (u32, spec::NumberMatch),
     },
     PitchBend {
+        channel: (u32, spec::NumberMatch),
         bend: (u32, spec::NumberMatch),
     },
 }
